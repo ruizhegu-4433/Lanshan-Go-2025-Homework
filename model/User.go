90f934f800150824c96eaf836311a52e8e9a08fc
ruizhegu-4433 <3247948963@qq.com>
2025-12-12 19:21:10 +0800

    Delete model directory

--- model/User.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type User struct {
-	ID       uint   `json:"id" gorm:"primaryKey;autoIncrement;not null"`
-	Username string `json:"username" gorm:"column:username;type:varchar(50);not null;unique"`
-	Password string `json:"password" gorm:"column:password;type:varchar(100);not null"`
-}
